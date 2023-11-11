0ad1359bfe72687ff5525f229adaf420e0b063ae
muzaffertcandan <muzaffertcandan@gmail.com>
2023-11-11 11:43:10 +0300

    ternary operator with states

--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,25 @@
-const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
-};
+import { useState } from "react";
 
+const ToggleChallenge = () => {
+  const [state, setState] = useState(false)
+  const Changer = () =>{
+    // state ? (setState(false)): (setState(true))
+    if(state){
+      setState(false)
+    }
+    else{
+      setState(true)
+    }
+  }
+  return (<>
+    <button className="btn" onClick={Changer}>Click me to change</button>
+    {state ? (<h1>here is a true element</h1>): <MyComponent/>}
+    
+  </>)
+};
+const MyComponent = ()=>{
+  return(
+    <h2>state false sooo I am returning a component YEYYYY</h2>
+  )
+}
 export default ToggleChallenge;
