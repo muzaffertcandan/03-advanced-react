da5d7ec380ec54d34fc4aba0999cf47551e8a3b3
muzaffertcandan <muzaffertcandan@gmail.com>
2023-11-12 18:47:14 +0300

    half

--- src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,20 +1,45 @@
+import { useState } from "react";
+import { data } from "../../../data";
 const UserChallenge = () => {
+  const [name, setName] = useState("");
+  const [users, setUsers] = useState(data);
+  console.log(users);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name);
+  };
+
   return (
     <div>
-      <form className='form'>
+      <form className="form" onSubmit={handleSubmit}>
         <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
       {/* render users below */}
+      <h4>Users</h4>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <h4>{user.name}</h4>
+          </div>
+        );
+      })}
     </div>
   );
 };
